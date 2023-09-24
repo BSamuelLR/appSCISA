d8319bba714d6f20616d7d92cf7234520acf9f4a
BRAYAN SAMUEL LOPEZ RIVERA <L20480120@nuevoleon.tecnm.mx>
2023-09-23 19:29:22 -0600

    gg

---
+++ appSCISA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace appSCISA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
